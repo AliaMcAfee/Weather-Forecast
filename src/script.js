f4e0804316f550c3ced849b67e7888056f4f624c
AliaMcAfee <mcafeealia@gmail.com>
2024-06-10 06:27:57 -0500

    Added Forecast

--- src/script.js
+++ src/script.js
@@ -51,7 +51,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">⛅</div>
+  <div class="weather-forecast-temperature">
+    <span class="temperature-max"><strong>15&deg;</strong></span>
+    <span class="temperature-min">9&deg;</span>
+  </div>  
+</div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Chicago");
+displayForecast();
